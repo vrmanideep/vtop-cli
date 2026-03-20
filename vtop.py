630a94e1dbb1d8d3bf4d28df74bac84bf0eff1c1
vrmanideep <vrmanideepphaniharam@gmail.com>
2026-03-20 20:52:56 +0530

    updated version to v5.0.1

--- vtop.py
+++ vtop.py
@@ -51,7 +51,7 @@ from vitap_vtop_client.client import VtopClient
 from services import *
 
 # --- Configuration ---
-CURRENT_VERSION = "5.0.0"
+CURRENT_VERSION = "5.0.1"
 
 def check_for_updates():
     # Imports kept inside so the updater is 100% self-contained
